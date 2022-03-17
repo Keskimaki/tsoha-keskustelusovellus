405df02ff5effae61ea4cd0c984f81b1f20e4219
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-17 15:50:09 +0200

    Create new users with POST

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,13 +1,27 @@
 """Router for user related api requests"""
 
+from flask import Response, request
+from werkzeug.security import generate_password_hash
+
 from app import app
-from services.db import query_db
+from services.db import query_db, insert_to_db
 from services.response import json_response
 
-@app.route("/api/users")
+@app.route("/api/users", methods=["GET"])
 def get_users():
     """Return all users as JSON"""
-
     users = query_db("SELECT * FROM Users;")
 
     return json_response(users)
+
+@app.route("/api/users", methods=["POST"])
+def create_user():
+    """Create a new user"""
+    body = request.json
+
+    insert_to_db(
+        "INSERT INTO Users (username, password) VALUES (%s, %s);",
+        ( body["username"], generate_password_hash(body["password"]) )
+    )
+
+    return Response(status=201)
