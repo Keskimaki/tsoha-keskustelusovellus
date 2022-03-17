421e2b7ea1af3800669e85ac6f8576d32d503765
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-17 14:01:42 +0200

    Initialize new tables in db

--- src/database/init_db.py
+++ src/database/init_db.py
@@ -7,15 +7,21 @@ from werkzeug.security import generate_password_hash
 
 from config import DB_PASSWORD
 from services.db import get_db_connection
-from queries import create_users_table
+from queries import create_users_table, create_boards_table, create_threads_table, create_posts_table
 
 conn = get_db_connection()
 
 cur = conn.cursor()
 
-cur.execute("DROP TABLE IF EXISTS users;")
+cur.execute("DROP TABLE IF EXISTS Posts;")
+cur.execute("DROP TABLE IF EXISTS Threads;")
+cur.execute("DROP TABLE IF EXISTS Boards;")
+#cur.execute("DROP TABLE IF EXISTS Users;")
 
 cur.execute(create_users_table)
+cur.execute(create_boards_table)
+cur.execute(create_threads_table)
+cur.execute(create_posts_table)
 
 cur.execute(
     "INSERT INTO Users (username, password, admin) VALUES (%s, %s, %s);",
