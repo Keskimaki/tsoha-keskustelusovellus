12f706e21d9ad43a492e7e0bfbab065ad3d9f582
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-21 10:34:41 +0200

    Update db connection config

--- src/config.py
+++ src/config.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USERNAME = os.getenv("DB_USERNAME")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
--- src/services/db.py
+++ src/services/db.py
@@ -2,14 +2,14 @@
 
 import psycopg2
 
-from config import DB_USERNAME, DB_PASSWORD
+from config import DB_NAME, DB_USER, DB_PASSWORD
 
 def get_db_connection():
     """Open database connection with user defined in .env"""
     conn = psycopg2.connect(
-        host = "localhost",
-        database = "tsoha",
-        user = DB_USERNAME,
+        host = "postgres",
+        database = DB_NAME,
+        user = DB_USER,
         password = DB_PASSWORD
     )
 
