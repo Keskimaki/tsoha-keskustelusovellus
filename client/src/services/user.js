863432e8f97dcc0c94471b3295753e6217a1eff8
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-22 13:58:00 +0200

    Add user creation service to frontend

---
+++ client/src/services/user.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+import { BASE_URI } from '../config'
+
+export const createUser = async (username, password) => {
+    const data = { username, password }
+    const res = await axios.post(`${BASE_URI}/users`, data)
+
+    return res.data
+}
