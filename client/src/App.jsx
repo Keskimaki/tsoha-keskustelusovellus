b74c931de28573e00c4505900eb3a8a16b8e3f58
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-22 15:38:59 +0200

    Refactor board view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
-import { getBoards } from './services/boards'
 
 import Boards from './components/Boards'
 import Threads from './components/Threads'
 import User from './components/User'
 import Login from './components/Login'
 
-const App = () => {
-  const [boards, setBoards] = useState([])
-
-  useEffect(async () => {
-    const data = await getBoards()
-    setBoards(data)
-  }, [])
-
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Boards boards={boards} />} />
-        <Route path="/:boardName" element={<Threads />} />
-        <Route path="/create" element={<User />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <Routes>
+      <Route path="/" element={<Boards />} />
+      <Route path="/:boardName" element={<Threads />} />
+      <Route path="/create" element={<User />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </div>
+)
 
 export default App
--- client/src/components/Boards.jsx
+++ client/src/components/Boards.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-const Boards = ({ boards }) => {
+import { getBoards } from '../services/boards'
+
+const Boards = () => {
+  const [boards, setBoards] = useState([])
+
+  useEffect(async () => {
+    const data = await getBoards()
+    setBoards(data)
+  }, [])
+
   return (
     <div>
       {boards.map(board =>
