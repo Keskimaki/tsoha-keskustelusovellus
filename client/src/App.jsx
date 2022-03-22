d36253d77fc9796af388c8bd9557aeff126df39d
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-22 11:07:09 +0200

    Connect to server with Axios and render board data in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [boards, setBoards] = useState([])
+
+  const getBoards = async () => {
+    const res = await axios.get('http://localhost:5000/api/boards')
+    setBoards(res.data)
+  }
+
+  useEffect(() => {
+    getBoards()
+  }, [])
+
   return (
     <div>
-      <p>Hello world</p>
+      {boards.map(board => <p key={board.name}>{board.name} {board.description}</p>)}
     </div>
   )
 }
