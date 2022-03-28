09515d2cd5a39abc58275dbca65cd11c6d1da050
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-28 12:20:43 +0300

    Add service for making threads

--- client/src/services/threads.js
+++ client/src/services/threads.js
@@ -1,11 +1,29 @@
 import axios from 'axios'
 
 import { BASE_URI } from '../config'
+import { makePost } from './posts'
 
 export const getThreads = async boardName => {
   const boardId = await getBoardId(boardName)
 
   const res = await axios.get(`${BASE_URI}/threads?board_id=${boardId}`)
+  return res.data
+}
+
+export const makeThread = async (token, userId, boardName, name, content) => {
+  const auth = { headers: { Authorization: token } }
+  const boardId = await getBoardId(boardName)
+
+  const newThread = {
+    user_id: userId,
+    board_id: boardId,
+    name
+  }
+
+  const res = await axios.post(`${BASE_URI}/threads`, newThread, auth)
+
+  await makePost(token, userId, name, content)
+
   return res.data
 }
 
