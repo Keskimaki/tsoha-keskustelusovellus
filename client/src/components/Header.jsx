6c118068694ff43f502cbc966792065ccc40124b
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-28 10:07:14 +0300

    Update header for logged user

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { HeaderWrapper, HeaderLink } from '../assets/styles'
+import { logoutUser } from '../services/login'
+import { UserContext } from './UserProvider'
 
-const Header = () => (
+const Header = () => {
+  const [user, setUser] = useContext(UserContext)
+
+  return (
     <HeaderWrapper>
-        <HeaderLink to="/">Forum</HeaderLink>
-        <HeaderLink to="/login">Login</HeaderLink>
-        <HeaderLink to="/create">Create Account</HeaderLink>
+      <HeaderLink to="/">Forum</HeaderLink>
+      {user
+        ? <>
+          <HeaderLink to="/" onClick={() => logoutUser(setUser)}>
+            Logout
+          </HeaderLink>
+          <HeaderLink to="/user">User</HeaderLink>
+        </>
+        : <>
+          <HeaderLink to="/login">Login</HeaderLink>
+          <HeaderLink to="/create">Create Account</HeaderLink>
+        </>
+      }
     </HeaderWrapper>
-)
+  )
+}
 
 export default Header
