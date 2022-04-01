4821a08853963b4472abc2a4c14a930e7dd82987
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-04-01 15:00:14 +0300

    Show post images

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Wrapper } from '../../assets/styles'
+import { Wrapper, Text, SecondaryText, Image, LargeImage } from '../../assets/styles'
+import { BASE_URI } from '../../config'
 
 const Post = ({ post }) => {
+  const [image, setImage] = useState(false)
+
   return (
       <Wrapper>
-        <strong>{post.username}</strong>
-        <br />
-        {post.content}
+        <strong>{post.username}</strong> <SecondaryText>{post.time}</SecondaryText>
+        <Text>{post.content}</Text>
+        {post.image && <br/>}
+        {post.image &&
+          (image
+            ? <Image onClick={() => setImage(!image)} src={`${BASE_URI}/images/${post.id}`} />
+            : <LargeImage onClick={() => setImage(!image)} src={`${BASE_URI}/images/${post.id}`} />
+          )}
       </Wrapper>
   )
 }
