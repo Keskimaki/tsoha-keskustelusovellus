dd7de08c5c38bffac1ae88ddc444b8a79b1b4616
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-04-02 11:52:37 +0300

    Update posts when adding when a new image

--- client/src/components/posts/AddImage.jsx
+++ client/src/components/posts/AddImage.jsx
@@ -3,12 +3,27 @@ import React from 'react'
 import { SmallButton } from '../../assets/styles'
 import { BASE_URI } from '../../config'
 
-const AddImage = ({ postId }) => {
+const AddImage = ({ postId, updatePosts, setImageFocus }) => {
+  const handleImageAddition = async () => {
+    setTimeout(async () => {
+      await updatePosts()
+      setImageFocus(false)
+    }, 1000)
+  }
+
   return (
     <div>
       <iframe name="dummy" id="dummy" style={{ display: 'none' }}></iframe>
-      <form target="dummy" action={`${BASE_URI}/images/${postId}`} method="POST" encType="multipart/form-data" >
-        <SmallButton type="submit">Upload</SmallButton>
+      <form
+        target="dummy"
+        action={`${BASE_URI}/images/${postId}`}
+        method="POST"
+        encType="multipart/form-data" >
+        <SmallButton
+          onClick={handleImageAddition}
+          type="submit">
+          Upload
+        </SmallButton>
         <input type="file" name="file" />
       </form>
     </div>
