13cf90a55b07dd0933fb685dba46010eaf07215a
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-04-02 10:31:54 +0300

    Add post deletion to frontend

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useContext } from 'react'
 
 import { Wrapper, Text, SecondaryText, SmallButton, Image, LargeImage } from '../../assets/styles'
-import { BASE_URI } from '../../config'
+import { deletePost } from '../../services/posts'
 import { UserContext } from '../UserProvider'
+import { BASE_URI } from '../../config'
 
-const Post = ({ post }) => {
+const Post = ({ post, updatePosts }) => {
   const [image, setImage] = useState(false)
   const [user] = useContext(UserContext)
 
@@ -12,24 +13,35 @@ const Post = ({ post }) => {
       <Wrapper>
         <strong>{post.username}</strong> <SecondaryText>{post.time}</SecondaryText>
         <Text>{post.content}</Text>
-        {user && user.id === post.user_id && <Buttons post={post} />}
+        {user && user.id === post.user_id && <Buttons post={post} updatePosts={updatePosts} />}
         {post.image &&
           (image
-            ? <Image onClick={() => setImage(!image)} src={`${BASE_URI}/images/${post.id}`} />
-            : <LargeImage onClick={() => setImage(!image)} src={`${BASE_URI}/images/${post.id}`} />
+            ? <LargeImage onClick={() => setImage(!image)} src={`${BASE_URI}/images/${post.id}`} />
+            : <Image onClick={() => setImage(!image)} src={`${BASE_URI}/images/${post.id}`} />
           )}
       </Wrapper>
   )
 }
 
-const Buttons = ({ post }) => {
+const Buttons = ({ post, updatePosts }) => {
+  const [user] = useContext(UserContext)
+
+  const handlePostDeletion = async () => {
+    if (!window.confirm('Are you sure you want to delete this post?')) {
+      return
+    }
+
+    await deletePost(user.token, post.id)
+    await updatePosts()
+  }
+
   return (
     <div>
       {post.image
         ? <SmallButton>Remove image</SmallButton>
         : <SmallButton>Add image</SmallButton>}
       <SmallButton>Edit</SmallButton>
-      <SmallButton>Delete</SmallButton>
+      <SmallButton onClick={handlePostDeletion}>Delete</SmallButton>
     </div>
   )
 }
