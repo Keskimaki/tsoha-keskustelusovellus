bfc6053fdb28f010ec38a8d14c4d76b7091c3ed1
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-04-05 14:07:33 +0300

    Display no posts message if user has not posted

--- client/src/components/user/index.jsx
+++ client/src/components/user/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { getPostsByUser } from '../../services/posts'
+import { Title } from '../../assets/styles'
 import Post from '../posts/post'
 
 const User = () => {
@@ -13,6 +14,10 @@ const User = () => {
     const data = await getPostsByUser(userId)
     setPosts(data.sort((a, b) => b.id - a.id))
   }, [])
+
+  if (posts.length === 0) {
+    return <Title>No Posts</Title>
+  }
 
   return (
     <div>
