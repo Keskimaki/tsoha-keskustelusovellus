5490ea8c3db12e5edb3874d600d3708b12b2f235
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-04-06 12:18:02 +0300

    Serve frontend files via backend

--- server/src/app.py
+++ server/src/app.py
@@ -1,13 +1,18 @@
 """Initialize and start the application"""
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 
 from config import JWT_SECRET_KEY
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder = "../../client/build/static",
+    template_folder = "../../client/build"
+)
+
 CORS(app)
 
 app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
@@ -23,3 +28,10 @@ from routes import boards
 from routes import threads
 from routes import posts
 from routes import images
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
